23bb1693e472fb070f668b50c6becef6ccd49d2e
graial <al@paglalayag.net>
2025-01-25 19:45:06 +0800

    tweak automation script

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -2,7 +2,7 @@ gsub_file("config/initializers.rb", /^((?!#.)end)/, "  init :bridgetown_theme_si
 
 prepend_to_file "frontend/styles/index.css" do
 	"@import \"bridgetown_theme_single_page_opt_in/frontend/styles/index.css\";"
-	"@import url("./lander.css");"
+	# "@import url("./lander.css");"
 end
 
 copy_file("spoi_data/data/1_top_banner.yml", "src/_data/1_top_banner.yml")
