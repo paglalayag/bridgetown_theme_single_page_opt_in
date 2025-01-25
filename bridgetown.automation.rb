b332c5e3363b2c26bdc85fff152116385c140b47
graial <al@paglalayag.net>
2025-01-25 19:33:04 +0800

    tweak automation script

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -1,9 +1,10 @@
-gsub_file("config/initializers.rb", "end", "init :bridgetown_theme_single_page_opt_in\nend")
+gsub_file("config/initializers.rb", /^((?!#.)end)/, "  init :bridgetown_theme_single_page_opt_in\nend")
 
-append_to_file "frontend/styles/index.css" do
+prepend_to_file "frontend/styles/index.css" do
 	"@import \"bridgetown_theme_single_page_opt_in/frontend/styles/index.css\";"
+	"@import url("./lander.css");"
 end
 
-# copy_file("spoi_data/data/1_top_banner.yml", "src/_data/1_top_banner.yml")
+copy_file("spoi_data/data/1_top_banner.yml", "src/_data/1_top_banner.yml")
 
 # Read the Automations documentation: https://www.bridgetownrb.com/docs/automations
