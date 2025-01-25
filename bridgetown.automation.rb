3669b21a5fd3fc5c765792198fdc13f5073af8bd
graial <al@paglalayag.net>
2025-01-25 18:58:33 +0800

    tweak automation file

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -1,9 +1,9 @@
-copy_file("spoi_data/data/1_top_banner.yml", "src/_data/1_top_banner.yml")
+gsub_file("config/initializers", "end", "init :bridgetown_theme_single_page_opt_in\nend")
 
 append_to_file "frontend/styles/index.css" do
 	"@import \"bridgetown_theme_single_page_opt_in/frontend/styles/index.css\";"
 end
 
-gsub_file("config/initializers", "end", "init :bridgetown_theme_single_page_opt_in\nend") 
+copy_file("spoi_data/data/1_top_banner.yml", "src/_data/1_top_banner.yml")
 
 # Read the Automations documentation: https://www.bridgetownrb.com/docs/automations
