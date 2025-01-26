e712d4617eab7b5cb79dac59211ad90fe2785618
graial <al@paglalayag.net>
2025-01-26 08:50:35 +0800

    replace front matter of index in setup automation

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -2,6 +2,7 @@ add_gem("bridgetown_theme_single_page_opt_in")
 add_gem("dotenv")
 
 gsub_file("config/initializers.rb", /^((?!#.)end)/, "  init :bridgetown_theme_single_page_opt_in\nend")
+gsub_file("layout: default", , "layout: bridgetown_theme_single_page_opt_in/landing")
 
 prepend_to_file "frontend/styles/index.css" do
 	"@import \"bridgetown_theme_single_page_opt_in/frontend/styles/index.css\";"
