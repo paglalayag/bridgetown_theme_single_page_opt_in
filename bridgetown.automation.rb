b0debf923b739781e3554e4a339eddda8727034f
graial <al@paglalayag.net>
2025-01-26 08:29:27 +0800

    tweak automation script

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -8,6 +8,4 @@ prepend_to_file "frontend/styles/index.css" do
 	# "@import url("./lander.css");"
 end
 
-copy_file(ENV["PROJECT_NAME"] + "/content/bridgetown_theme_single_page_opt_in/1_top_banner.yml", "src/_data/1_top_banner.yml")
-
 # Read the Automations documentation: https://www.bridgetownrb.com/docs/automations
